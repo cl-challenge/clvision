4274a20a9b29df82855860192306fb3bc04fedb9
solangii <solangii@unist.ac.kr>
2022-05-27 19:10:54 +0900

    [chore]: resolve conflict

--- utils/custom_plugin.py
+++ utils/custom_plugin.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import torch
-import torch.nn as nn
-from avalanche.core import SupervisedPlugin, Template
 import numpy as np
 import random
-from utils.cutmix_utils import onehot, rand_bbox
-
-=======
 import torch.nn as nn
 from avalanche.core import SupervisedPlugin, Template
-from data import CutMix
->>>>>>> 1d5f88f831f770b9383b19c7b20d0b70ed391ca9
 
 class EvalMode(SupervisedPlugin):
     def __init__(self, start_exp=1):
@@ -34,17 +26,12 @@ class EvalMode(SupervisedPlugin):
 
 
 class CutMixPlugin(SupervisedPlugin):
-<<<<<<< HEAD
     def __init__(self, num_class, beta, prob, num_mix, device='cuda'):
-=======
-    def __init__(self, num_class, beta, prob, num_mix):
->>>>>>> 1d5f88f831f770b9383b19c7b20d0b70ed391ca9
         super().__init__()
         self.num_class = num_class
         self.beta = beta
         self.prob = prob
         self.num_mix = num_mix
-<<<<<<< HEAD
         self.device = device
 
     def before_training_iteration(self, strategy: Template, *args, **kwargs):
@@ -54,7 +41,7 @@ class CutMixPlugin(SupervisedPlugin):
 
         for index in range(length):
             img, lb = strategy.mb_x[index], strategy.mb_y[index]
-            lb_onehot = onehot(self.num_class, lb).to(self.device)
+            lb_onehot = self.onehot(self.num_class, lb).to(self.device)
 
             for _ in range(self.num_mix):
                 r = np.random.rand(1)
@@ -68,7 +55,7 @@ class CutMixPlugin(SupervisedPlugin):
                 img2, lb2 = strategy.mb_x[rand_index], strategy.mb_y[rand_index]
                 lb2_onehot = onehot(self.num_class, lb2).to(self.device)
 
-                bbx1, bby1, bbx2, bby2 = rand_bbox(img.size(), lam)
+                bbx1, bby1, bbx2, bby2 = self.rand_bbox(img.size(), lam)
                 img[:, bbx1:bbx2, bby1:bby2] = img2[:, bbx1:bbx2, bby1:bby2]
                 lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (img.size()[-1] * img.size()[-2]))
                 lb_onehot = lb_onehot * lam + lb2_onehot * (1. - lam)
@@ -77,20 +64,37 @@ class CutMixPlugin(SupervisedPlugin):
         strategy.mbatch[0], strategy.mbatch[1] = data, label
         # strategy.mb_x, strategy.mb_y = data, label
 
+    def onehot(self, size, target):
+        vec = torch.zeros(size, dtype=torch.float32)
+        vec[target] = 1.
+        return vec
+
+    def rand_bbox(self, size, lam):
+        if len(size) == 4:
+            W = size[2]
+            H = size[3]
+        elif len(size) == 3:
+            W = size[1]
+            H = size[2]
+        else:
+            raise Exception
+
+        cut_rat = np.sqrt(1. - lam)
+        cut_w = np.int(W * cut_rat)
+        cut_h = np.int(H * cut_rat)
+
+        # uniform
+        cx = np.random.randint(W)
+        cy = np.random.randint(H)
+
+        bbx1 = np.clip(cx - cut_w // 2, 0, W)
+        bby1 = np.clip(cy - cut_h // 2, 0, H)
+        bbx2 = np.clip(cx + cut_w // 2, 0, W)
+        bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+        return bbx1, bby1, bbx2, bby2
 
 __all__ = [
     'EvalMode',
     'CutMixPlugin'
-=======
-
-    def train_dataset_adaptation(self, **kwargs):
-        self.adapted_dataset = CutMix(self.experience.dataset, num_class=self.num_class, beta=self.beta, prob=self.prob,
-                                      num_mix=self.num_mix)
-        self.adapted_dataset.current_transform_group = 'train'
-
-    # def after_train_dataset_adaptation(self, s):
-
-__all__ = [
-    'EvalMode'
->>>>>>> 1d5f88f831f770b9383b19c7b20d0b70ed391ca9
 ]
